d600e15e4c54b81814b72fee3185c3d0e7bf4259
lsr <imlimr1232@gmail.com>
2020-06-20 12:19:49 +0900

    add mongoose

--- server.js
+++ server.js
@@ -7,12 +7,23 @@ const morgan = require("morgan");
 //body-parser 불러와서 상수화
 const bodyParser = require("body-parser");
 
+const mongoose = require("mongoose");
+
 // express 일반적인 기능들을 상수화
 const app = express();
 
 // 라우터파일 불러오기
 const productRoute = require("./routes/products");
 const orderRoute = require("./routes/orders");
+
+// 데이터베이스 연결
+const dbAddress =
+  "mongodb+srv://admin:12345@cluster0-45mth.mongodb.net/shoppingmalldb?retryWrites=true&w=majority";
+
+mongoose
+  .connect(dbAddress, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDb Connected ..."))
+  .catch((err) => console.log(err));
 
 // 미들웨어 설정
 app.use(morgan("dev"));
