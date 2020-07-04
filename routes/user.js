1adc285b579cabb22408a7951e9a9aeb35e6638b
lsr <imlimr1232@gmail.com>
2020-07-04 15:59:07 +0900

    이메일 체크유무 확인, password 암호화

--- routes/user.js
+++ routes/user.js
@@ -6,42 +6,60 @@ const userModel = require("../model/user");
 
 // 3 회원가입
 router.post("/register", (req, res) => {
-  // password 암호화
-  bcrypt.hash(req.body.password, 10, (err, hash) => {
-    // password 암호화 과정중 에러 발생시
-    if (err) {
-      // 에러가 난 경우 else (불필요한 빌드를) 거치지 않기 위해 return 을 사용한다
-      return res.json({
-        message: err.message,
-      });
-    } else {
-      // 정상적으로 암호화를 거친경우 db에 저장함.
-      const user = new userModel({
-        username: req.body.userName,
-        email: req.body.email,
-        password: hash,
-      });
-
-      user
-        .save()
-        .then((result) => {
-          res.json({
-            message: "success created user",
-            userInfo: {
-              id: result._id,
-              username: result.username,
-              email: result.email,
-              password: result.password,
-            },
-          });
-        })
-        .catch((err) => {
-          res.json({
-            error: err.message,
-          });
+  // 이메일 체크유무 확인 from database
+  userModel
+    .findOne({ email: req.body.email })
+    .then((user) => {
+      // database에 사용자가 있는 경우 에러 메시지를 뿌려줌.
+      if (user) {
+        return res.json({
+          message: "email already exists",
         });
-    }
-  });
+      } else {
+        // email이 없는 경우에는 database에 저장
+        // password 암호화
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          // password 암호화 과정중 에러 발생시
+          if (err) {
+            // 에러가 난 경우 else (불필요한 빌드를) 거치지 않기 위해 return 을 사용한다
+            return res.json({
+              message: err.message,
+            });
+          } else {
+            // 정상적으로 암호화를 거친경우 db에 저장함.
+            const user = new userModel({
+              username: req.body.userName,
+              email: req.body.email,
+              password: hash,
+            });
+
+            user
+              .save()
+              .then((result) => {
+                res.json({
+                  message: "success created user",
+                  userInfo: {
+                    id: result._id,
+                    username: result.username,
+                    email: result.email,
+                    password: result.password,
+                  },
+                });
+              })
+              .catch((err) => {
+                res.json({
+                  error: err.message,
+                });
+              });
+          }
+        });
+      }
+    })
+    .catch((err) => {
+      res.json({
+        error: err.message,
+      });
+    });
 });
 
 // 4 로그인
