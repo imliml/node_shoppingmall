dc76ea312d69fe7f689bc9ce77c90039d8d2d371
lsr <imlimr1232@gmail.com>
2020-07-04 14:40:40 +0900

    order better response updated...

--- routes/orders.js
+++ routes/orders.js
@@ -15,7 +15,15 @@ router.post("/", (req, res) => {
     .then((result) => {
       res.json({
         message: "order product",
-        orderProduct: result,
+        orderProduct: {
+          id: result._id,
+          product: result.product,
+          quantity: result.quantity,
+          request: {
+            type: "GET",
+            url: "http://localhost:5000/order/" + result._id,
+          },
+        },
       });
     })
     .catch((err) => {
@@ -29,12 +37,49 @@ router.post("/", (req, res) => {
 router.get("/", (req, res) => {
   orderModel
     .find()
-    .populate("product", ["name", "price"])
+    .populate("product", ["name", "price"]) // product의 하위 메뉴를 보여줌.
     .then((docs) => {
-      res.json({
-        message: "order total get",
+      const response = {
         count: docs.length,
-        orders: docs,
+        orders: docs.map((doc) => {
+          return {
+            id: doc._id,
+            product: doc.product,
+            quantity: doc.quantity,
+            request: {
+              type: "GET",
+              url: "http://localhost:5000/order/" + doc._id,
+            },
+          };
+        }),
+      };
+      res.json(response);
+    })
+    .catch((err) => {
+      res.json({
+        error: err.message,
+      });
+    });
+});
+
+// 4-1 디테일한 order 데이터를 불러오는 api
+router.get("/:id", (req, res) => {
+  const orderId = req.params.id;
+
+  orderModel
+    .findById(orderId)
+    .then((doc) => {
+      res.json({
+        message: "order detail get",
+        orderInfo: {
+          id: doc._id,
+          product: doc.product,
+          quantity: doc.quantity,
+          request: {
+            type: "GET",
+            url: "http://localhost5000:/order",
+          },
+        },
       });
     })
     .catch((err) => {
@@ -45,17 +90,52 @@ router.get("/", (req, res) => {
 });
 
 // 5 order 데이터 수정하는 api
-router.patch("/", (req, res) => {
-  res.json({
-    message: "order update",
-  });
+router.patch("/:id", (req, res) => {
+  const orderId = req.params.id;
+
+  const updatedOps = {};
+  for (const ops of req.body) {
+    updatedOps[ops.propName] = ops.value;
+  }
+
+  orderModel
+    .findByIdAndUpdate(orderId, { $set: updatedOps })
+    .then((result) => {
+      res.json({
+        message: "updated order",
+        request: {
+          type: "GET",
+          url: "http://localhost:5000/order/" + orderId,
+        },
+      });
+    })
+    .catch((err) => {
+      res.json({
+        error: err.message,
+      });
+    });
 });
 
 // 6 order 데이터 삭제하는 api
-router.delete("/", (req, res) => {
-  res.json({
-    message: "order delete",
-  });
+router.delete("/:id", (req, res) => {
+  const orderId = req.params.id;
+
+  orderModel
+    .findByIdAndDelete(orderId)
+    .then(() => {
+      res.json({
+        message: "order delete",
+        request: {
+          type: "GET",
+          url: "http://localhost:5000/order",
+        },
+      });
+    })
+    .catch((err) => {
+      res.json({
+        error: err.message,
+      });
+    });
 });
 
 // 2
